fcecb1876adefddd88c3da8fb7cd994313b4bb0c
Ahmed Fahim Foysal Eishon <ahmed-fahim-foysal-eishon@ib-system.co.jp>
2021-10-13 10:59:07 +0900

    ios module cloned

--- ios/Classes/FlutterBarcodeScanPlugin.h
+++ ios/Classes/FlutterBarcodeScanPlugin.h
@@ -1,4 +1,14 @@
 #import <Flutter/Flutter.h>
+#import "BarcodeScannerViewControllerDelegate.h"
 
-@interface FlutterBarcodeScanPlugin : NSObject<FlutterPlugin>
+#define UIColorFromRGB(rgbValue) \
+[UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+  green:((float)((rgbValue & 0x00FF00) >>  8))/255.0 \
+  blue:((float)((rgbValue & 0x0000FF) >>  0))/255.0 \
+  alpha:1.0]
+
+@interface FlutterBarcodeScanPlugin : NSObject<FlutterPlugin, BarcodeScannerViewControllerDelegate>
+
+@property(nonatomic, copy) FlutterResult result;
+@property (nonatomic, assign) UIViewController *hostViewController;
 @end
--- ios/Classes/FlutterBarcodeScanPlugin.m
+++ ios/Classes/FlutterBarcodeScanPlugin.m
@@ -6,15 +6,40 @@
       methodChannelWithName:@"flutter_barcode_scan"
             binaryMessenger:[registrar messenger]];
   FlutterBarcodeScanPlugin* instance = [[FlutterBarcodeScanPlugin alloc] init];
+  instance.hostViewController = [UIApplication sharedApplication].delegate.window.rootViewController;
   [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else if ([@"scan" isEqualToString:call.method]) {
+        self.result = result;
+        [self showBarcodeView];
   } else {
     result(FlutterMethodNotImplemented);
   }
+}
+
+- (void)showBarcodeView {
+    BarcodeScannerViewController *scannerViewController = [[BarcodeScannerViewController alloc] init];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:scannerViewController];
+    scannerViewController.delegate = self;
+    [self.hostViewController presentViewController:navigationController animated:NO completion:nil];
+}
+
+- (void)barcodeScannerViewController:(BarcodeScannerViewController *)controller didScanBarcodeWithResult:(NSString *)result {
+    if (self.result) {
+        self.result(result);
+    }
+}
+
+- (void)barcodeScannerViewController:(BarcodeScannerViewController *)controller didFailWithErrorCode:(NSString *)errorCode {
+    if (self.result){
+        self.result([FlutterError errorWithCode:errorCode
+                                        message:nil
+                                        details:nil]);
+    }
 }
 
 @end
